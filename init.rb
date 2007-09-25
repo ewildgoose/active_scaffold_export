afed2c600e4a5c35f4d282db91df79a8b94a266f
Daniel Einspanjer <deinspanjer@gmail.com>
2007-09-25 17:19:31 +0000

    Patch from Ana to prevent possible ambiguous reference to load method.

--- init.rb
+++ init.rb
@@ -2,11 +2,11 @@
 ActiveScaffold rescue throw "should have included ActiveScaffold plug in first.  Please make sure that this plug-in comes alphabetically after the ActiveScaffold plug-in"
 
 # Load our overrides
-load 'actions/export.rb'
-load 'config/export.rb'
-load 'config/core.rb'
-load 'helpers/view_helpers.rb'
-load 'helpers/export_helpers.rb'
+Kernel::load 'actions/export.rb'
+Kernel::load 'config/export.rb'
+Kernel::load 'config/core.rb'
+Kernel::load 'helpers/view_helpers.rb'
+Kernel::load 'helpers/export_helpers.rb'
 
 ##
 ## Run the install script, too, just to make sure
