5a505ba6833d097e11ec883626c0de0d7a6ed009
Ed Wildgoose <git@wildgooses.com>
2008-07-24 17:25:38 +0100

    Added additional AS stylesheet functions to support using <%stylesheet_link_tag active_scaffold_stylesheets, my_style, :cache => true%>

--- lib/helpers/view_helpers_override.rb
+++ lib/helpers/view_helpers_override.rb
@@ -8,6 +8,19 @@ module ActiveScaffold
         active_scaffold_includes_without_export + "\n" + css + "\n<!--[if IE]>" + ie_css + "<![endif]-->\n"
       end
       alias_method_chain :active_scaffold_includes, :export
+      
+      # Provides stylesheets to include with +stylesheet_link_tag+
+      def active_scaffold_stylesheets_with_export(frontend = :default)
+        active_scaffold_stylesheets_without_export.to_a << ActiveScaffold::Config::Core.asset_path("export-stylesheet.css", frontend)
+      end
+      alias_method_chain :active_scaffold_stylesheets, :export
+      
+      # Provides stylesheets for IE to include with +stylesheet_link_tag+ 
+      def active_scaffold_ie_stylesheets_with_export(frontend = :default)
+        active_scaffold_ie_stylesheets_without_export.to_a << ActiveScaffold::Config::Core.asset_path("export-stylesheet-ie.css", frontend)
+      end
+      alias_method_chain :active_scaffold_ie_stylesheets, :export
+      
     end
   end
 end
